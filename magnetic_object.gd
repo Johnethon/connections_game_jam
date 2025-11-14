df77f3d4c9b700cc1d45d5f8bcd3156e3d07ea0d
Johnethon <98053051+Johnethon@users.noreply.github.com>
2025-11-14 10:43:07 -0500

    return of locked axis and rotation

--- magnetic_object.gd
+++ magnetic_object.gd
@@ -5,9 +5,18 @@ class_name MagneticObject
 @export var both : bool = false
 
 @export var lock_x : bool = false
-@export var lock_z : bool = false
+@export var lock_y : bool = false
+
+var base_x = 0
+var base_y = 0
 
 func _ready():
+	
+	if lock_x:
+		base_x = global_position.x
+		
+	if lock_y:
+		base_y = global_position.y
 	
 	##collides with wire
 	collision_layer += 1 << 1
@@ -18,4 +27,11 @@ func _ready():
 	collision_mask += 1 << 3
 
 func _integrate_forces(state: PhysicsDirectBodyState2D) -> void:
-	pass
+	
+	rotation = 0.0
+	
+	if lock_x:
+		global_position.x = base_x
+	
+	if lock_y:
+		global_position.y = base_y

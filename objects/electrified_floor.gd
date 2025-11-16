2d81bf2dce37013f5a856c2081665d754ff5bd25
Nightshade2023 <150634815+Nightshade2023@users.noreply.github.com>
2025-11-16 15:52:16 -0500

    lvl3step1

--- objects/electrified_floor.gd
+++ objects/electrified_floor.gd
@@ -1,7 +1,19 @@
 extends Powerable
+@export var Negative : bool = false
+
+func _ready():
+	super()
+	if Negative:
+		removed_power()
 
 func given_power():
-	$Node.do_damage = true
+	if Negative:
+		$Node.do_damage = false
+	else:
+		$Node.do_damage = true
 
 func removed_power():
-	$Node.do_damage = false
+	if Negative:
+		$Node.do_damage = true
+	else:
+		$Node.do_damage = false
